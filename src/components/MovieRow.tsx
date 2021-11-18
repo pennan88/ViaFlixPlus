f3ef74da5dfd060965f326ccd06964dd81218712
Alexnader Fagrelius <alexander.fagrelius@drapereurope.se>
2021-11-18 10:38:58 +0100

    .

--- src/components/MovieRow.tsx
+++ src/components/MovieRow.tsx
@@ -10,56 +10,30 @@ interface iProps {
 
 export const MovieRow: React.FC<iProps> = ({ value, key, image }) => {
   const [movies, setMovies] = useState([]);
-  const [movieId, setMovieId] = useState("");
+  const [loading, setLoading] = useState(false);
 
   var options = {
     method: "GET",
-    url: "https://imdb8.p.rapidapi.com/title/get-most-popular-movies",
-    params: { homeCountry: "US", purchaseCountry: "US", currentCountry: "US" },
-    headers: {
-      "x-rapidapi-host": "imdb8.p.rapidapi.com",
-      "x-rapidapi-key": "089092a969msha15ee252d78de78p16f86fjsnb34df78677ef",
-    },
+    url: "https://imdb-api.com/en/API/Top250Movies/k_5er7itwc",
   } as any;
 
   useEffect(() => {
-    async function fetchMovieId() {
-      axios
-        .request(options)
-        .then(function (response) {
-          setMovieId(response.data);
-          console.log()
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-
-      for (let i = 0; i < movieId.length; i++) {
-        const element = movieId[i];
-        console.log(element)
-        var movieOptions = {
-          method: "GEt",
-          url: `https://imdb-api.com/en/API/Title/k_5er7itwc/${element}`,
-        } as any;
-        axios
-        .request(movieOptions)
-        .then(function (response) {
-          setMovies(response.data);
-          console.log(response.data);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-      }
-
-      movies.map((value) => {
-          console.log(value)
-      })
-    
-    }
-    fetchMovieId();
+    fetchData();
   }, []);
 
+  function fetchData() {
+    setLoading(true);
+    axios
+      .request(options)
+      .then(function (response) {
+        setMovies(response.data.items);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  console.log(movies);
   return (
     <div key={key} className="genreContainer">
       <p className="genreTag">{value?.description}</p>
@@ -77,13 +51,13 @@ export const MovieRow: React.FC<iProps> = ({ value, key, image }) => {
           <img alt="error" src={image} />
         </div>
         <div className="imageInnerContainer">
-          <img  alt="error"src={image} />
+          <img alt="error" src={image} />
         </div>
         <div className="imageInnerContainer">
-          <img  alt="error"src={image} />
+          <img alt="error" src={image} />
         </div>
         <div className="imageInnerContainer">
-          <img  alt="error"src={image} />
+          <img alt="error" src={image} />
         </div>
         <div className="imageInnerContainer">
           <img alt="error" src={image} />
